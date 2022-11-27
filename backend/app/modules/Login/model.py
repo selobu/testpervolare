246f538de1213968bb3ee8c6a552a91bf12d34e9
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-27 11:17:41 -0500

    Specify requeriments depends on what the user prefer to do: test, install, generate documentation. Adding initial github workflow

--- backend/app/modules/Login/model.py
+++ backend/app/modules/Login/model.py
@@ -7,7 +7,9 @@ from pydantic import EmailStr, validator
 from hashlib import sha256
 
 import re
+
 Tb = settings.app.Tb
+
 
 @map_name_to_table
 class Login(SQLModel, table=True):
@@ -15,11 +17,11 @@ class Login(SQLModel, table=True):
     email: EmailStr = Field(sa_column=Column(String(50)))
     password: str = Field(sa_column=Column(String(300)))
     user_id: Optional[str] = Field(default=None, foreign_key="user.id")
-    
+
     @validator(password)
     def pass_validator(cls, v):
         assert len(v) >= 8 and len(v) <= 16
-        r = ''.join(re.findall('[0-9]*[a-zA-Z]*[0-9]*[\*\/\+\-\$\%\&]*',v))
+        r = "".join(re.findall("[0-9]*[a-zA-Z]*[0-9]*[\*\/\+\-\$\%\&]*", v))
         assert len(r) != len(v)
         # the system stores only the sha256 for security reasons
-        return sha256(r.encode('utf-8')).hexdigest()
+        return sha256(r.encode("utf-8")).hexdigest()
