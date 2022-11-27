791c6a1cd2114e8c2d24d2b8aaf0b869a6a75c5e
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-27 05:20:27 -0500

    apply black to config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,13 +1,16 @@
 from pydantic import BaseSettings
 from os import environ
 
-adminpassword = environ.get('MYSQL_ROOT_PASSWORD')
-adminuser = environ.get('MYSQL_ROOT_USER')
-port = environ.get('MYSQL_PORT')
-database = environ.get('MYSQL_DATABASE',)
-host = environ.get('MYSQL_HOST')
-user = environ.get('MYSQL_USER')
-userpassword = environ.get('MYSQL_PASSWORD')
+adminpassword = environ.get("MYSQL_ROOT_PASSWORD")
+adminuser = environ.get("MYSQL_ROOT_USER")
+port = environ.get("MYSQL_PORT")
+database = environ.get(
+    "MYSQL_DATABASE",
+)
+host = environ.get("MYSQL_HOST")
+user = environ.get("MYSQL_USER")
+userpassword = environ.get("MYSQL_PASSWORD")
+
 
 class Settings(BaseSettings):
     api_name: str = "Backend api"
@@ -16,11 +19,16 @@ class Settings(BaseSettings):
     admin_email: str = ""
     items_per_user: int = 50
     database_uri: str = "sqlite:///database.db"
-    api_contact: object = {'name': 'Sebastian López Buriticá', 'email': 'sebastian.lopez@gestionhseq.com',
-                        'url': 'https://gestionhseq.com'}
-    database_user_uri: str = f"mysql+pymysql://{user}:{userpassword}@{host}:{port}/{database}"
-    database_maria_uri: str = f"mysql+pymysql://{adminuser}:{adminpassword}@{host}:{port}/{database}"
-    app: object={}
-    engine: object={}
+    api_contact: object = {
+        "name": "Sebastian López Buriticá",
+        "email": "sebastian.lopez@gestionhseq.com",
+        "url": "https://gestionhseq.com",
+    }
+    database_user_uri: str = (
+        f"mysql+pymysql://{user}:{userpassword}@{host}:{port}/{database}"
+    )
+    app: object = {}
+    engine: object = {}
+
 
 settings = Settings()
