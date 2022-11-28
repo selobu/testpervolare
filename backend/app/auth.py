9d80e7e4ebc8a09056eb379cedc9d169e2036a47
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-28 05:25:43 -0500

    fix auth black

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -16,7 +16,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     email = form_data.username
     password = form_data.password
     with Session(settings.engine) as session:
-        res = select(Tb.Login.password).filter(Tb.Login.email==email)
+        res = select(Tb.Login.password).filter(Tb.Login.email == email)
         currpass = session.exec(res).first()
     if currpass is None:
         raise HTTPException(status_code=400, detail="Incorrect useremail or password")
