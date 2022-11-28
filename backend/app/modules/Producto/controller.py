a3939d70859ec15b7f03eaefcdd5e21fc12652ca
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-28 11:26:53 -0500

    Apply Product.description constrains

--- backend/app/modules/Producto/controller.py
+++ backend/app/modules/Producto/controller.py
@@ -8,7 +8,7 @@ class Producto(BaseModel):
     id: Optional[str]= Field(description='UUID in the format xxxxx-xxxxx-xxxx-xxxx')
     name: str = Field(description='Product name, only alphanumeric values')
     value: int = Field(description='Float values with maximum 10 digits included decimal point readed as a point')
-    description: Optionslstr = Field(description='')
+    description: str = Field(description='Product description', max_length=500, min_length=10)
     @validator("name")
     def name_validator(cls, v):
         res = "".join(re.findall("([0-9]*[a-zA-Z]+[0-9]*)", v))
@@ -23,8 +23,7 @@ class Producto(BaseModel):
 
     @validator("description")
     def desc_validator(cls, v):
-        assert len(v) >= 10 and len(v) <= 500
-        res = "".join(re.findall("([0-9]*[a-zA-Z]+[0-9]*)", v))
+        res = "".join(re.findall("([0-9]*[a-zA-Z]*[0-9]*)", v))
         if len(res) != len(v):
             raise ValueError("only allowed charactes and numerical values")
         return v
