751119ad8b91115dcbbdf34809ea32d44e7e099f
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-28 21:29:50 -0500

    black applied

--- backend/tests/test_tools.py
+++ backend/tests/test_tools.py
@@ -1,9 +1,25 @@
 from app import tools
 
 
-def test_tools_force_check():pass
-def test_tools_digest(): pass
-def test_tools_uuid_isvalid(): pass
-def test_tools_paginate_parameters():pass
-def test_tools_map_name_to_table():pass
-def test_tools_generate_random():pass
+def test_tools_force_check():
+    pass
+
+
+def test_tools_digest():
+    pass
+
+
+def test_tools_uuid_isvalid():
+    pass
+
+
+def test_tools_paginate_parameters():
+    pass
+
+
+def test_tools_map_name_to_table():
+    pass
+
+
+def test_tools_generate_random():
+    pass
