e528925ec904ebc2ad18f5e9264f3de4972c1dcc
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-29 09:40:09 -0500

    add navbar

--- frontend/product/src/components/HelloWorld.vue
+++ frontend/product/src/components/HelloWorld.vue
@@ -1,37 +1,17 @@
 <template>
+  <NavBar title="Products application"></NavBar>
   <v-container class="fill-height">
+    
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="src/assets/logo.svg"
-      />
+      <v-img contain height="125" src="src/assets/logo.svg" />
 
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <h1 class="text-h2 font-weight-bold">Products</h1>
 
       <div class="py-14" />
 
       <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://next.vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
         <v-col cols="auto">
           <v-btn
             color="primary"
@@ -42,31 +22,22 @@
             target="_blank"
             variant="flat"
           >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-speedometer" size="large" start />
 
             Get Started
           </v-btn>
         </v-col>
-
         <v-col cols="auto">
           <v-btn
-            href="https://community.vuetifyjs.com/"
+            href="https://gestionhseq.com/"
             min-width="164"
             rel="noopener noreferrer"
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-account-group" size="large" start />
 
-            Community
+            Main page
           </v-btn>
         </v-col>
       </v-row>
@@ -74,6 +45,11 @@
   </v-container>
 </template>
 
-<script setup>
-  //
+<script>
+import Navbar from "./navbar.vue";
+export default {
+  components: {
+    NavBar: Navbar,
+  },
+};
 </script>
