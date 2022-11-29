6ee0d3ecca9a4d997f0a267902404b287a7993a3
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-29 11:46:01 -0500

    signin sigup routes

--- frontend/product/src/router/index.js
+++ frontend/product/src/router/index.js
@@ -2,6 +2,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Products from '@/views/Products.vue'
+import Signin from '@/views/signin.vue'
+import Signup from '@/views/signup.vue'
+
 
 const routes = [
   {
@@ -13,6 +16,16 @@ const routes = [
     path: '/products',
     name: 'Products',
     component: Products
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
