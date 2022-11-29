12b816cbd86439f18ebaea7610543c0bf6eda4db
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-29 09:12:17 -0500

    added vuetify dependency

--- frontend/product/src/App.vue
+++ frontend/product/src/App.vue
@@ -1,19 +1,26 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/navbar.vue";
+
+export default {
+  components: { HelloWorld, Navbar },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-contaniner>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Hello world" />
+  </v-contaniner>
 </template>
 
 <style scoped>
---
+++ frontend/product/src/components/navbar.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+    props: { title: { type: String, required: true } },
+};
+</script>
+<template>
+    <v-toolbar density="comfortable" :title="title"></v-toolbar>
+</template>
