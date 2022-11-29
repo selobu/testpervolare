b4defa181b7735986986e2619eec6c012753f39f
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-29 06:08:14 -0500

    black applied

--- backend/tests/test_tools.py
+++ backend/tests/test_tools.py
@@ -2,23 +2,28 @@ import pytest
 from app import tools
 from json import dumps
 
+
 def test_tools_force_check():
     pass
 
 
 def test_tools_digest():
-    assert tools.digest('1232312312321344523') == 'fcfc29e5897720bd34e9ceb43e4938c3982405afec65cb8ca56d510587b7ba92'
+    assert (
+        tools.digest("1232312312321344523")
+        == "fcfc29e5897720bd34e9ceb43e4938c3982405afec65cb8ca56d510587b7ba92"
+    )
 
 
 def test_tools_uuid_isvalid():
-    assert tools.uuid_isvalid('16fd2706-8baf-433b-82eb-8c7fada847da')
+    assert tools.uuid_isvalid("16fd2706-8baf-433b-82eb-8c7fada847da")
+
 
 @pytest.mark.trio
 async def test_tools_paginate_parameters():
-    q = 'hellostr'
-    skip=2
-    limit=5
-    r1 = await tools.paginate_parameters(q,skip,limit)
+    q = "hellostr"
+    skip = 2
+    limit = 5
+    r1 = await tools.paginate_parameters(q, skip, limit)
     assert dumps(r1) == dumps({"q": q, "skip": skip, "limit": limit})
 
 
@@ -27,5 +32,5 @@ def test_tools_map_name_to_table():
 
 
 def test_tools_generate_random():
-    for i in range(2,20):
+    for i in range(2, 20):
         assert len(tools.generate_random(i)) == i
