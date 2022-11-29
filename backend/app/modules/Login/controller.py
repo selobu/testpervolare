41459f3b4016b0ac0c85939c4af18fad1a35b39a
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-28 19:33:37 -0500

    black applied

--- backend/app/modules/Login/controller.py
+++ backend/app/modules/Login/controller.py
@@ -2,11 +2,13 @@ from pydantic import validator, BaseModel, EmailStr, Field
 from tools import map_2_pydantic, digest
 import re
 
+
 @map_2_pydantic
 class Login(BaseModel):
     email: EmailStr = Field(description="Your email")
     password: str = Field(description="Password", max_length=16, min_length=8)
-    @validator('password')
+
+    @validator("password")
     def pass_validator(cls, v):
         assert len(v) >= 8 and len(v) <= 16
         r = "".join(re.findall("[0-9]*[a-zA-Z]*[0-9]*[\*\/\+\-\$\%\&]*", v))
