88b2e2d7c2301825952445029e43c23f2ebe7ad5
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-29 08:44:27 -0500

    adding vuetify as ui

--- frontend/product/src/components/HelloWorld.vue
+++ frontend/product/src/components/HelloWorld.vue
@@ -13,24 +13,13 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://hv.gestionhseq.com" target="_blank">my cv </a>, for details
+    and explanations
   </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
